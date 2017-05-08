8027c595ae63434248b6a8d875896eaca34e76a0
andy <andy47110@gmail.com>
2017-05-08 22:56:26 +0800

    變更Homework資料夾路徑

---
+++ Homework/MVC5Homework1/MVC5Homework1/Controllers/CustDataManageController.cs
@@ -0,0 +1,21 @@
+﻿using MVC5Homework1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Homework1.Controllers
+{
+    public class CustDataManageController : Controller
+    {
+        客戶資料Entities db = new 客戶資料Entities();
+        // GET: CustDataManage
+        public ActionResult Index()
+        {
+            var all = db.客戶資料.AsQueryable();
+            
+            return View();
+        }
+    }
+}
