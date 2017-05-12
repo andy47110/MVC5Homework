bcce961bed1e4be5c3c8695b6de78cdac95896db
andy <andy47110@gmail.com>
2017-05-12 23:58:16 +0800

    儲存SQL查詢

---
+++ DoItagain/Week1/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿	SELECT 
+		客戶名稱,
+		Email,
+		(SELECT Count(*) FROM DBO.客戶聯絡人 c WHERE c.id = Cust.id) AS 聯絡人數量,
+		(SELECT COUNT(*) FROM DBO.客戶銀行資訊 b WHERE b.Id=Cust.Id) AS 銀行帳戶數量
+	FROM dbo.客戶資料 Cust
